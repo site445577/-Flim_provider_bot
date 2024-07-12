4fb4c35139c7a11251db37547fdc35f697e44f34
teraboxmoviebot <teraboxmoviebot@gmail.com>
2024-07-13 00:28:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -28,7 +28,7 @@ I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ
 ➪ /batch › Tᴏ Cʀᴇᴀᴛᴇ Lɪɴᴋ Fᴏʀ Mᴜʟᴛɪᴘʟᴇ Mᴇᴅɪᴀ
 
 <b>⪼ EG:</b>
-</code>/batch https://t.me/cinemaxxxxxxxxxxx1 https://t.me/cinemaxxxxxxxxxxx/10</code>"""
+</code>/batch https://t.me/cinemaxxxxxxxxxxx/1 https://t.me/cinemaxxxxxxxxxxx/10</code>"""
   
     FILTER_TXT = "Sᴇʟᴇᴄᴛ Wʜɪᴄʜ Oɴᴇ Yᴏᴜ Wᴀɴᴛ...✨"
     
